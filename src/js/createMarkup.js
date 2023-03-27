5c94f512285130a6e73509e0fd6838772b16efa5
bakmaiev <thisyoungrhyme@gmail.com>
2023-03-27 12:21:34 +0300

    move the markup to another file

--- src/js/createMarkup.js
+++ src/js/createMarkup.js
@@ -1,3 +1,33 @@
-export function createMarkup() {
-  console.log;
+export function renderCountryList(countries) {
+  return countries
+    .map(country => {
+      return `
+          <li class='country-list__item'>
+            <img src='${country.flags.svg}' alt='${country.name.official}' width='30'>
+            <p>${country.name.official}</p>
+          </li>
+      `;
+    })
+    .join('');
+}
+
+export function renderCountryInfo(countries) {
+  return countries
+    .map(country => {
+      return `
+      <h1 class='country-list__title'>
+        <img src='${country.flags.svg}' 
+        alt='${country.name.official}' width='30'>
+        <p>${country.name.official}</p> 
+      </h1> 
+      <ul> 
+        <li class='country-list__item'>Capital: ${country.capital}</li> 
+        <li class='country-list__item'>Population: ${country.population}</li> 
+        <li class='country-list__item'>Languages: ${Object.values(
+          country.languages
+        )}</li> 
+      </ul>
+      `;
+    })
+    .join('');
 }
